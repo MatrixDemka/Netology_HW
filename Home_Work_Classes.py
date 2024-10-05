846136bf6874597059c5721263bcfd66741e4f7b
Aleksey Demin <matrixdemka@yandex.ru>
2024-10-05 18:45:10 +0300

    modifymodified thr code

--- Home_Work_Classes.py
+++ Home_Work_Classes.py
@@ -6,6 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.average_grade_course = {}
     
     def rate_lecturer(self, lecturer, course, grade):
         """метод оценки лекторов"""
@@ -25,6 +26,8 @@ class Student:
         for i in self.grades.values():
             value.extend(i)
         return round(sum(value)/len(value), 2)
+    
+    
     
     def __str__(self) -> str:
         """метод __str__ выводит тнформацио по данному классу"""
@@ -115,7 +118,7 @@ student2.courses_in_progress += ["Python"] # Второй учится на ку
 student2.finished_courses += ["Java"]  # Закончил курс Java
 
 student3 = Student("Denia", "Armstrong", 18)
-student3.courses_in_progress += ["Java"]  # Третий учится на курсе Java
+student3.courses_in_progress += ["Java", "Python"]  # Третий учится на курсе Java
 student3.finished_courses += ["Python"]  # Закончил курс Python
 
 #  Создаём двух проверяющих
@@ -132,15 +135,22 @@ lecturer.courses_attached += ["Python"] # Закреплён за курсом P
 lecturer2 = Lecturer("Lisa", "Semenova")
 lecturer2.courses_attached += ["Java"] # Закреплён за курсом Java
 
-#  Проверяющий 1 выставляет оценку студенту 1
+#  Проверяющий выставляет оценку студенту
 reviewer1.rate_hw(student1, "Java", 5)
 reviewer1.rate_hw(student1, "Java", 8)
 reviewer1.rate_hw(student1, "Java", 10)
 
-#  Проверяющий 2 выставляет оценку студенту 2
+reviewer1.rate_hw(student3, "Java", 5)
+reviewer1.rate_hw(student3, "Java", 8)
+reviewer1.rate_hw(student3, "Java", 10)
+
 reviewer2.rate_hw(student2, "Python", 5)
 reviewer2.rate_hw(student2, "Python", 8)
 reviewer2.rate_hw(student2, "Python", 10)
+
+reviewer2.rate_hw(student3, "Python", 3)
+reviewer2.rate_hw(student3, "Python", 6)
+reviewer2.rate_hw(student3, "Python", 8)
 
 reviewer2.rate_hw(student3, "Python", 3)
 reviewer2.rate_hw(student3, "Python", 6)
@@ -156,36 +166,49 @@ student2.rate_lecturer(lecturer, "Python", 10)
 student2.rate_lecturer(lecturer, "Python", 8)
 student2.rate_lecturer(lecturer, "Python", 4)
 
-print("Поверяющий 1 выставляет оценки студенту 1")
-print(student1.grades)
+# print("Поверяющий 1 выставляет оценки студенту 1")
+# print(student1.grades)
 
-print("Поверяющий 2 выставляет оценки студенту 2")
-print(student2.grades)
+# print("Поверяющий 2 выставляет оценки студенту 2")
+# print(student2.grades)
 
-print("Студент 2 выставляет оценку лектору 1")
-print(lecturer.grades)
+# print("Студент 2 выставляет оценку лектору 1")
+# print(lecturer.grades)
 
-print("Студент 1 выставляет оценку лектору 2")
-print(lecturer2.grades)
+# print("Студент 1 выставляет оценку лектору 2")
+# print(lecturer2.grades)
 
-print(student1)
-print(reviewer1)
-print(lecturer)
+# print(student1)
+# print(reviewer1)
+# print(lecturer)
 
-print(student1.__eq__(student2))
-print(lecturer.__eq__(lecturer2))
-print(student1.__lt__(student2))
-print(lecturer.__le__(lecturer2))
+# print(student1.__eq__(student2))
+# print(lecturer.__eq__(lecturer2))
+# print(student1.__lt__(student2))
+# print(lecturer.__le__(lecturer2))
 
 
 all_student_lst = [student1 , student2, student3]
-#all_lecturer_lst = [best_lecturer_1, best_lecturer_2, best_lecturer_3]
- 
-def get_average_grade(all_student_lst, course_name):
-        count = []
-        for student in all_student_lst:
-            if student.courses_in_progress == [course_name]:
-                count.extend(student)
-        return sum(all_student_lst.average_grade())/len(count)
 
-print(student1.get_average_grade(all_student_lst, 'Python'))
+def average_grade_on_the_course(persons, course):
+    if not isinstance(persons, list):
+        return "Not list"
+    all_average_grade = []
+    for person in persons:
+        all_average_grade.extend(person.average_grade_course.get(course, []))
+        if not all_average_grade:
+            return "По такому курсу ни у кого нет оценок"
+    return round(sum(all_average_grade) / len(all_average_grade), 2)
+
+print(average_grade_on_the_course(all_student_lst, "Python"))
+
+# #all_lecturer_lst = [best_lecturer_1, best_lecturer_2, best_lecturer_3]
+ 
+# def get_average_grade(all_student_lst, course_name):
+#         count = []
+#         for student in all_student_lst:
+#             if student.courses_in_progress == [course_name]:
+#                 count.extend(student)
+#         return sum(all_student_lst.average_grade())/len(count)
+
+# print(student1.get_average_grade(all_student_lst, 'Python'))
