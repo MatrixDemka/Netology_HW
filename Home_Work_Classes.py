cd5fdbd6d4528d2653d2cbe4df1850c467c58baa
Aleksey Demin <matrixdemka@yandex.ru>
2024-10-07 17:40:53 +0300

    added function average_grade_on_the_course

--- Home_Work_Classes.py
+++ Home_Work_Classes.py
@@ -26,9 +26,7 @@ class Student:
         for i in self.grades.values():
             value.extend(i)
         return round(sum(value)/len(value), 2)
-    
-    
-    
+         
     def __str__(self) -> str:
         """метод __str__ выводит тнформацио по данному классу"""
         courses_in_progress_string = ", ".join(self.courses_in_progress)  # Получаем строку курсов
@@ -105,10 +103,21 @@ class Reviewer(Mentor):
         result = f"Имя: {self.name} \n" \
         f"Фамилия: {self.surname}"
         return result
-        
-        
 
-#  Создаём двух студентов
+"""
+Функция подсчета средней оценки по всем (студентам/лекторам) в рамках конкретного курса
+""" 
+def average_grade_on_the_course(persons, course):
+    count = []
+    summa =[]
+    for person in persons:
+        for course in person.grades.keys():
+            count = len(person.grades[course])
+            summa = sum(person.grades[course])
+    return round(summa / count, 2)
+
+
+#Создаём студентов
 student1 = Student("Anna", "Karenina", 22)
 student1.courses_in_progress += ["Java"]  # Первый учится на курсе Java
 student1.finished_courses += ["Python"]  # Закончил курс Python
@@ -118,24 +127,27 @@ student2.courses_in_progress += ["Python"] # Второй учится на ку
 student2.finished_courses += ["Java"]  # Закончил курс Java
 
 student3 = Student("Denia", "Armstrong", 18)
-student3.courses_in_progress += ["Java", "Python"]  # Третий учится на курсе Java
+student3.courses_in_progress += ["Java"]  # Третий учится на курсе Java
 student3.finished_courses += ["Python"]  # Закончил курс Python
 
-#  Создаём двух проверяющих
+#Создаём проверяющих
 reviewer1 = Reviewer("Zigmund", "Freid")
 reviewer1.courses_attached += ["Java"] # Закреплён за курсом Java
 
 reviewer2 = Reviewer("Jasson", "Blue")
 reviewer2.courses_attached += ["Python"] # Закреплён за курсом Python
 
-#  Создаём двух лекторов
+#Создаём лекторов
 lecturer = Lecturer("Anna", "Smith")
 lecturer.courses_attached += ["Python"] # Закреплён за курсом Python
 
 lecturer2 = Lecturer("Lisa", "Semenova")
 lecturer2.courses_attached += ["Java"] # Закреплён за курсом Java
 
-#  Проверяющий выставляет оценку студенту
+lecturer3 = Lecturer("Dina", "Ignatova")
+lecturer3.courses_attached += ["Python", "Java"] # Закреплён за курсом Python
+
+#Проверяющие выставляют оценки студентам
 reviewer1.rate_hw(student1, "Java", 5)
 reviewer1.rate_hw(student1, "Java", 8)
 reviewer1.rate_hw(student1, "Java", 10)
@@ -152,63 +164,58 @@ reviewer2.rate_hw(student3, "Python", 3)
 reviewer2.rate_hw(student3, "Python", 6)
 reviewer2.rate_hw(student3, "Python", 8)
 
-reviewer2.rate_hw(student3, "Python", 3)
-reviewer2.rate_hw(student3, "Python", 6)
+reviewer2.rate_hw(student3, "Python", 10)
+reviewer2.rate_hw(student3, "Python", 4)
 reviewer2.rate_hw(student3, "Python", 8)
 
-# Студент 1 выставляет оценку лектору 2
+#Студенты выставляют оценки лекторам
 student1.rate_lecturer(lecturer2, "Java", 6)
 student1.rate_lecturer(lecturer2, "Java", 2)
 student1.rate_lecturer(lecturer2, "Java", 4)
 
-#  Студент 2 выставляет оценку лектору 1
 student2.rate_lecturer(lecturer, "Python", 10)
 student2.rate_lecturer(lecturer, "Python", 8)
 student2.rate_lecturer(lecturer, "Python", 4)
 
-# print("Поверяющий 1 выставляет оценки студенту 1")
-# print(student1.grades)
+student3.rate_lecturer(lecturer3, "Java", 10)
+student3.rate_lecturer(lecturer3, "Java", 7)
+student3.rate_lecturer(lecturer3, "Java", 5)
 
-# print("Поверяющий 2 выставляет оценки студенту 2")
-# print(student2.grades)
+print("Поверяющий 1 выставляет оценки студенту 1")
+print(student1.grades)
+print()
+print("Поверяющий 2 выставляет оценки студенту 2")
+print(student2.grades)
+print()
+print("Студент 2 выставляет оценку лектору 1")
+print(lecturer.grades)
+print()
+print("Студент 1 выставляет оценку лектору 2")
+print(lecturer2.grades)
+print()
+print("Студент 1 выставляет оценку лектору 3")
+print(lecturer3.grades)
+print()
+print(student1)
+print()
+print(reviewer1)
+print()
+print(lecturer)
+print()
+print(student1.__eq__(student2))
+print(lecturer.__eq__(lecturer2))
+print(student1.__lt__(student2))
+print(lecturer.__le__(lecturer2))
 
-# print("Студент 2 выставляет оценку лектору 1")
-# print(lecturer.grades)
+all_student_lst = [student1, 
+                   student2, 
+                   student3
+                   ]
 
-# print("Студент 1 выставляет оценку лектору 2")
-# print(lecturer2.grades)
+all_lecturer_list = [lecturer,
+                     lecturer2,
+                     lecturer3
+                     ]
 
-# print(student1)
-# print(reviewer1)
-# print(lecturer)
-
-# print(student1.__eq__(student2))
-# print(lecturer.__eq__(lecturer2))
-# print(student1.__lt__(student2))
-# print(lecturer.__le__(lecturer2))
-
-
-all_student_lst = [student1 , student2, student3]
-
-def average_grade_on_the_course(persons, course):
-    if not isinstance(persons, list):
-        return "Not list"
-    all_average_grade = []
-    for person in persons:
-        all_average_grade.extend(person.average_grade_course.get(course, []))
-        if not all_average_grade:
-            return "По такому курсу ни у кого нет оценок"
-    return round(sum(all_average_grade) / len(all_average_grade), 2)
-
-print(average_grade_on_the_course(all_student_lst, "Python"))
-
-# #all_lecturer_lst = [best_lecturer_1, best_lecturer_2, best_lecturer_3]
- 
-# def get_average_grade(all_student_lst, course_name):
-#         count = []
-#         for student in all_student_lst:
-#             if student.courses_in_progress == [course_name]:
-#                 count.extend(student)
-#         return sum(all_student_lst.average_grade())/len(count)
-
-# print(student1.get_average_grade(all_student_lst, 'Python'))
+print(f'Средняя оценка студентов - {average_grade_on_the_course(all_student_lst, "Python")}')
+print(f'Средняя оценка лекторов - {average_grade_on_the_course(all_lecturer_list, "Python")}')
